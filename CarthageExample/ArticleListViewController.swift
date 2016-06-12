61eaef32fad58d92994c58521aacacc3f089e69c
Yoshio HANAWA <y@hnw.jp>
2016-06-13 00:48:16 +0900

    Implement Qiita viewer with Alamofire and SwiftyJSON

--- CarthageExample/ArticleListViewController.swift
+++ CarthageExample/ArticleListViewController.swift
@@ -2,21 +2,56 @@
 //  ArticleListViewController.swift
 //  CarthageExample
 //
-//  Created by hnw on 2016/06/13.
+//  Created by hnw on 2016/06/12.
 //  Copyright © 2016年 hnw. All rights reserved.
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ArticleListViewController: UIViewController {
-    let table = UITableView() // プロパティにtableを追加
+class ArticleListViewController: UIViewController, UITableViewDataSource {
+    let table = UITableView()
+    var articles: [[String: String?]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "新着記事"
 
-        title = "新着記事" // Navigation Barのタイトルを設定
+        table.frame = view.frame
+        view.addSubview(table)
+        table.dataSource = self
 
-        table.frame = view.frame // tableの大きさをviewの大きさに合わせる
-        view.addSubview(table) // viewにtableを乗せる
+        getArticles()
+    }
+
+    func getArticles() {
+        Alamofire.request(.GET, "https://qiita.com/api/v2/items")
+            .responseJSON { response in
+                guard let object = response.result.value else {
+                    return
+                }
+                let json = JSON(object)
+                json.forEach({ (_, json) in
+                    let article: [String: String?] = [
+                        "title": json["title"].string,
+                        "userId": json["user"]["id"].string
+                    ]
+                    self.articles.append(article)
+                })
+                self.table.reloadData()
+        }
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]!
+        cell.detailTextLabel?.text = article["userId"]!
+        return cell
     }
 }
